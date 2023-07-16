30a7bd00927944241037538bb164f68dcb5010e7
thoyala <98438367+thoyala@users.noreply.github.com>
2023-07-16 15:02:53 +0700

    week 3-4
    combine week 3 and 4

---
+++ app/Http/Controllers/MyProfileController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyProfileController extends Controller
+{
+    public function create(Request $request)
+    {
+        $name = $request->get('name');
+        $lastname = $request->get('lastname');
+
+        $fullname = $name . " " . $lastname;
+        $sensor_name = str_replace("a", "*", $fullname);
+        echo $sensor_name;
+
+        return view("myprofile/create");
+    }
+
+    public function edit($id)
+    {
+        $profile = (object)[
+            "id" => $id,
+            "name" => "James",
+            "lastname" =>  "Mars",
+            "email" => "james@vru.ac.th",
+        ];
+        $others = "hello world";
+        return view("myprofile/edit", compact('profile', 'others'));
+    }
+
+    public function show($id)
+    {
+        $profile = (object)[
+            "id" => $id,
+            "name" => "James",
+            "lastname" =>  "Mars",
+            "email" => "james@vru.ac.th",
+        ];
+        $others = "hello world";
+        return view("myprofile/show", compact('profile', 'others'));
+    }
+
+    public function coronavirus(){
+        $reports = [
+            (object) ["country"=>"China" , "date"=>"2020-04-19" , "total"=>"2765", "active"=>"790"  , "death"=>"47", "recovered"=>"1928"],
+            (object) ["country"=>"Thailand" , "date"=>"2020-04-18" , "total"=>"2733", "active"=>"899"  , "death"=>"47", "recovered"=>"1787"],
+            (object) ["country"=>"Thailand" , "date"=>"2020-04-17" , "total"=>"2700", "active"=>"964"  , "death"=>"47", "recovered"=>"1689"],
+            (object) ["country"=>"Thailand" , "date"=>"2020-04-16" , "total"=>"2672", "active"=>"1033" , "death"=>"46", "recovered"=>"1593"],
+            (object) ["country"=>"Thailand" , "date"=>"2020-04-15" , "total"=>"2643", "active"=>"1103" , "death"=>"43", "recovered"=>"1497"],
+        ];
+        return view("myprofile/coronavirus", compact("reports") );
+    }
+
+    public function gallery (){
+        $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+        $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
+        $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+        $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
+        $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
+    
+        return view("newgallery/index", compact("ant", "bird", "cat", "god", "spider"));
+    }
+
+    public function ant (){
+        $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+
+    
+        return view("newgallery/ant", compact("ant"));
+    }
+
+    public function bird (){
+        $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
+    
+        return view("newgallery/bird", compact("bird"));
+    }
+
+    public function cat (){
+        $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+    
+        return view("newgallery/cat", compact("cat"));
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,14 +42,14 @@ Route::get('/greeting', function () {
     return view('greeting', compact('name', 'last_name'));
 });
 
-Route::get( "/gallery" , function(){
-	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-$bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg"; 
-$cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-$god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg"; 
-$spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg"; 
+Route::get("/gallery", function () {
+    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
+    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
+    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
 
-return view("test/index", compact("ant","bird","cat","god","spider") );
+    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
 });
 
 Route::get("/gallery/ant", function () {
@@ -66,3 +67,51 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
+Route::get("/teacher", function () {
+    return view("teacher");
+});
+
+Route::get("/student", function () {
+    return view("student");
+});
+
+Route::get("/theme", function () {
+    return view("theme");
+});
+
+// Route Template Inheritance
+Route::get("/teacher/inheritance", function () {
+    return view("teacher-inheritance");
+});
+Route::get("/student/inheritance", function () {
+    return view("student-inheritance");
+});
+
+// Route Template Component
+Route::get("/teacher/component", function () {
+    return view("teacher-component");
+});
+Route::get("/student/component", function () {
+    return view("student-component");
+});
+
+Route::get('/tables', function () {
+    return view('tables');
+});
+
+Route::get("/table/component", function () {
+    return view("table-component");
+});
+
+Route::get("/myprofile/create", [MyProfileController::class, "create"]);
+
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
+
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+
+Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
+
+Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
+Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
+Route::get( "/newgallery/bird" , [ MyProfileController::class , "bird" ] );
+Route::get( "/newgallery/cat" , [ MyProfileController::class , "cat" ] );
